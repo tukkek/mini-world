b5a4c53419add0059c5e68bd3bba3913e3a0b607
Alex Henry <tukkek@gmail.com>
2024-10-14 00:00:00 +0000

    RNG seed.

--- code/control/client.js
+++ code/control/client.js
@@ -1,12 +1,21 @@
 import * as worldm from '../model/world.js'
 import * as names from '../model/names.js'
 import * as map from '../view/map.js'
+import * as rpg from './rpg.js'
+
+const SEED=new URL(document.location).searchParams.get('seed')
 
 var world=false
 
 export function show(area){map.draw(area.cells)}
 
 export async function setup(){
+  let l=document.location
+  if(!SEED){
+    l.href+=`?seed=${new Date().toISOString().replace(/[^0-9]/g, '').slice(0,4+2+2+2+2)}`
+    return
+  }
+  rpg.seed(SEED)
   await names.setup()
   world=new worldm.World()
   show(world)
